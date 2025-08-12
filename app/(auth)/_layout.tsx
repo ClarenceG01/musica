1111954fe590715523c88d19db3d58ff1cdfa029
ClarenceG01 <gatama001@gmail.com>
2025-08-13 00:05:53 +0300

    signup page ui

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,20 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect, Stack } from "expo-router";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AuthLayout() {
   const { isSignedIn } = useAuth();
   if (isSignedIn) {
     return <Redirect href="/(tabs)" />;
   }
-  return <Stack />;
+  return (
+    <SafeAreaView className="flex-1">
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeAreaView>
+  );
 }
